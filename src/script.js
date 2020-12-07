e9f8a76f0e597da4338592cefeb368f0243efe6d
nebobyeoli <nikkilover17@naver.com>
2020-12-07 19:10:06 +0900

    + upgrade.html && rename index.html >> latest.html
    + linking in script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
 document.querySelector('button.quizlet').addEventListener('click', () => {
-    window.location.reload();
+    window.location.replace('./latest.html');
+});
+
+document.querySelector('button.labeled.upgrade').addEventListener('click', () => {
+    window.location.replace('./upgrade.html');
 });
