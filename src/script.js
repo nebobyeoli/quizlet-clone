46a8ba49918229b624e9c00d4349a6d95af4f9e9
nebobyeoli <nikkilover17@naver.com>
2020-12-08 08:24:19 +0900

    + Privacy Policy | Clone of Quizlet

--- src/script.js
+++ src/script.js
@@ -1,4 +1,7 @@
-try {
+try
+{
+    // redirect
+
     document.querySelector('button.quizlet').addEventListener('click', () => {
         window.location.replace('./latest.html');
     });
@@ -6,8 +9,11 @@ try {
     document.querySelector('button.labeled.upgrade').addEventListener('click', () => {
         window.location.replace('./upgrade.html');
     });
+    
+    document.querySelector('span#privacy-policy').parentElement.addEventListener('click', () => {
+        window.location.replace('./privacy.html');
+    });
 }
-
 catch (err) {
     console.error(err);
 }
