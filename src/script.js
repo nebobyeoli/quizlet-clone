b459a32745f9861546210b4dd4ce8096970bacd7
nebobyeoli <nikkilover17@naver.com>
2020-12-07 21:12:34 +0900

    Format upgrade.html
    + upgrade.css
    + added trycatch on append.js, script.js
    
    STUDY, CREATE, FOCUS
    
    GET QUIZLET PLUS AND STREAMLINE YOUR STUDYING
    
    im not sorry

--- src/script.js
+++ src/script.js
@@ -1,7 +1,13 @@
-document.querySelector('button.quizlet').addEventListener('click', () => {
-    window.location.replace('./latest.html');
-});
+try {
+    document.querySelector('button.quizlet').addEventListener('click', () => {
+        window.location.replace('./latest.html');
+    });
 
-document.querySelector('button.labeled.upgrade').addEventListener('click', () => {
-    window.location.replace('./upgrade.html');
-});
+    document.querySelector('button.labeled.upgrade').addEventListener('click', () => {
+        window.location.replace('./upgrade.html');
+    });
+}
+
+catch (err) {
+    console.error(err);
+}
